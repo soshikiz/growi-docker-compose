44a76a4cc5dea3897210758cc017061d1762a05f
root <root@soshikiz-wiki.asia-northeast1-b.c.soshikiz-wiki.internal>
2019-01-27 12:18:53 +0000

    add restore.sh

---
+++ restore.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BACKUP_PATH=/home/growi/backup
+MONGO_CONTAINER_NAME=growi_mongo_1
+GROWI_CONTAINER_NAME=growi_app_1
+NETWORK_CONTAINER_NAME=growi_default
+
+sudo docker run -it --rm --link ${MONGO_CONTAINER_NAME} --network ${NETWORK_CONTAINER_NAME} --volume ${BACKUP_PATH}:/backup mongo:3.4 bash -c "mongorestore -v --host ${MONGO_CONTAINER_NAME} --db growi backup/growi"
+sudo docker cp  ${BACKUP_PATH}/data ${GROWI_CONTAINER_NAME}:/data

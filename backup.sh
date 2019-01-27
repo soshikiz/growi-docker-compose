fb4c2669f138d0ec5a1c4f3be3f9cf5a7d84d0ee
root <root@soshikiz-wiki.asia-northeast1-b.c.soshikiz-wiki.internal>
2019-01-27 11:46:07 +0000

    add gitignore. fix backup.sh, docker-compose.yml

--- backup.sh
+++ backup.sh
@@ -2,16 +2,16 @@
 
 TODAY=`date +"%Y%m%d"`
 BACKUP_PATH=/home/growi/backup
-MONGO_CONTAINERID=2664abab
-GROWI_CONTAINERID=2e0e2bf
+MONGO_CONTAINER_NAME=growi_mongo_1
+GROWI_CONTAINER_NAME=growi_app_1
+NETWORK_CONTAINER_NAME=growi_default
 
-sudo docker cp ${MONGO_CONTAINERID}:/data/db ${BACKUP_PATH}/db
-sudo docker cp ${GROWI_CONTAINERID}:/data ${BACKUP_PATH}/data
+sudo docker run -it --rm --link ${MONGO_CONTAINER_NAME} --network ${NETWORK_CONTAINER_NAME} --volume ${BACKUP_PATH}:/backup mongo:3.4 bash -c "mongodump --host ${MONGO_CONTAINER_NAME} --db growi --out /backup"
+sudo docker cp ${GROWI_CONTAINER_NAME}:/data ${BACKUP_PATH}/data
 
-sudo tar cvf ${BACKUP_PATH}/${TODAY}growi_mongodb.tar ${BACKUP_PATH}/db
-sudo tar cvf ${BACKUP_PATH}/${TODAY}growi_uploaded.tar ${BACKUP_PATH}/data
+sudo tar -C ${BACKUP_PATH} -zcvf ${BACKUP_PATH}/${TODAY}_growi_backup.tar.gz growi data
 
-sudo rm -rf ${BACKUP_PATH}/db
+sudo rm -rf ${BACKUP_PATH}/growi
 sudo rm -rf ${BACKUP_PATH}/data
 
 find ${BACKUP_PATH} -name "*.tar" -mtime +7 -delete
